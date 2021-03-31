a031ae49dc983a237a691db3c8968cbb3e59ac75
google-boy <otienoh13@gmail.com>
2021-03-31 10:08:29 +0300

    Initial commit.

---
+++ action.js
@@ -0,0 +1,21 @@
+
+        let deadline = new Date("August 9, 2022 11:59:59").getTime()
+        let timer = setInterval(() => {
+            let now = new Date().getTime()
+            let difference = deadline - now
+
+            let days = Math.floor(difference / (1000 * 60 * 60 * 24))
+            let hours = Math.floor(difference % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+            let minutes = Math.floor(difference % (1000 * 60 * 60) / (1000 * 60))
+            let seconds = Math.floor(difference % (1000 * 60) / 1000)
+
+            document.getElementById('days').innerHTML = days
+            document.getElementById('hours').innerHTML = hours
+            document.getElementById('minutes').innerHTML = minutes
+            document.getElementById('seconds').innerHTML = seconds
+
+            if (difference < 0) {
+                clearInterval(timer)
+                document.getElementById('countdown').innerHTML = "TIME OUT!"
+            }
+        }, 1000)
